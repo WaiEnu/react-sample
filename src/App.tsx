ac0c9f1d9e3a945964969d86647ae0f50963e507
WaiEnu <53822854+WaiEnu@users.noreply.github.com>
2020-12-16 20:22:17 +0900

    20201216

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import PageA from "./PageA";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/page_a" exact component={PageA} />
-    </BrowserRouter>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/page_a">Page A</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" exact component={Home} />
+        <Route path="/page_a" exact component={PageA} />
+      </div>
+    </Router>
   );
 }
 
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <Link to="/page_a">PageA</Link>
     </div>
   );
 }
