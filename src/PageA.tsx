685dc65c111602bbecf169741ae27122f325ed83
WaiEnu <53822854+WaiEnu@users.noreply.github.com>
2020-12-16 20:36:50 +0900

    20201216

--- src/PageA.tsx
+++ src/PageA.tsx
@@ -2,6 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function PageA() {
+  const today: Date = new Date();
+  console.log(today);
+  const year = today.getFullYear().toString();
+  const month = today.getMonth().toString();
+  const day = today.getDay().toString();
+  const date = today.getDate().toString();
+  //today.getHours()
+  //today.getMinutes()
+  //today.getSeconds()
+  console.log(year + "/" + month + "/" + date + "/" + day);
   return (
     <div>
       <h1>PageA</h1>
