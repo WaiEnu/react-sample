3c498d24813266db1ce0ba72848776e25fc520ba
WaiEnu <strayhituji@gmail.com>
2021-02-05 12:29:59 +0900

    パス問題解決

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-    // some configuration
-    assetPrefix: process.env.NODE_ENV === "production" ? "/react-sample" : '',
-    basePath: process.env.NODE_ENV === 'production' ? '/react-sample' : '',
-    // another configuration
+    assetPrefix: "/react-sample",
 };
