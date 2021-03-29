7761eae2cde365eaae29099aefc4adb0d12aff4d
WaiEnu <strayhituji@gmail.com>
2021-03-29 16:44:34 +0900

    [fix]

--- pages/kanban/index.tsx
+++ pages/kanban/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import Pane from '../../components/templates/Pane'
-import { Timer } from '../../components/molecules/Timer';
 
 export default function Watch() {
   return (
